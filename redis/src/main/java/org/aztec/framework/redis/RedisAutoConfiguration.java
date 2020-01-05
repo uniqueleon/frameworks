5a18114603a8051ce3c46b636da1e1f165860d7d
uniqueleon <dawnfarrel@126.com>
2020-01-05 12:01:35 +0800

    commit

--- redis/src/main/java/org/aztec/framework/redis/RedisAutoConfiguration.java
+++ redis/src/main/java/org/aztec/framework/redis/RedisAutoConfiguration.java
@@ -1,5 +1,16 @@
 package org.aztec.framework.redis;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+@Configuration
 public class RedisAutoConfiguration {
 
+	@Bean
+	  public LettuceConnectionFactory redisConnectionFactory() {
+
+	    return new LettuceConnectionFactory(new RedisStandaloneConfiguration("server", 6379));
+	  }
 }
