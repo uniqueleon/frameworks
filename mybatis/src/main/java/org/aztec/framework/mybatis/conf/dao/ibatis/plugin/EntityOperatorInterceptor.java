54cf802ead7799e5af9a3a03e223b9cc0fd3599d
liming <liming11@sunfun.com>
2020-03-17 15:13:58 +0800

    commit

--- mybatis/src/main/java/org/aztec/framework/mybatis/conf/dao/ibatis/plugin/EntityOperatorInterceptor.java
+++ mybatis/src/main/java/org/aztec/framework/mybatis/conf/dao/ibatis/plugin/EntityOperatorInterceptor.java
@@ -1,11 +1,14 @@
 package org.aztec.framework.mybatis.conf.dao.ibatis.plugin;
 
+import java.lang.reflect.Array;
 import java.lang.reflect.Field;
 import java.sql.Timestamp;
+import java.util.List;
 import java.util.Properties;
 
 import javax.persistence.Table;
 
+import org.apache.ibatis.binding.MapperMethod.ParamMap;
 import org.apache.ibatis.executor.Executor;
 import org.apache.ibatis.mapping.MappedStatement;
 import org.apache.ibatis.mapping.SqlCommandType;
@@ -14,6 +17,9 @@ import org.apache.ibatis.plugin.Intercepts;
 import org.apache.ibatis.plugin.Invocation;
 import org.apache.ibatis.plugin.Plugin;
 import org.apache.ibatis.plugin.Signature;
+import org.apache.ibatis.session.defaults.DefaultSqlSession.StrictMap;
+import org.aztec.framework.mybatis.conf.dao.ibatis.ShardingThreadLocalContext;
+import org.aztec.framework.mybatis.conf.dao.ibatis.ShardingThreadLocalContext.ShardingColumnMatchType;
 import org.aztec.framework.mybatis.utils.CustomTimeOperatingUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,6 +27,11 @@ import org.springframework.context.annotation.Configuration;
 
 import io.shardingsphere.core.keygen.DefaultKeyGenerator;
 
+/**
+ * 实体操作拦截器。用于自动注入生成的主键ID
+ * @author liming
+ *
+ */
 @Configuration
 @Intercepts({ @Signature(type = Executor.class, method = "update", args = { MappedStatement.class, Object.class }) })
 public class EntityOperatorInterceptor implements Interceptor {
@@ -37,31 +48,99 @@ public class EntityOperatorInterceptor implements Interceptor {
         SqlCommandType sqlCommandType = mappedStatement.getSqlCommandType();
         if (SqlCommandType.INSERT.equals(sqlCommandType)) {
             object = invocation.getArgs()[1];
-            Field idField = object.getClass().getDeclaredField("id");
-            Table tableAnnotation = object.getClass().getAnnotation(Table.class);
-            if (tableAnnotation == null || idField == null)
-                return;
-            idField.setAccessible(true);
-            Long id =  defaultKeyGenerator.generateKey().longValue();
-            //idField.set(object, dailySequenceManager.incr(tableAnnotation.name()));
-            idField.set(object, id);
+            Class objClass = object.getClass();
+            System.out.println(object);
+            System.out.println(objClass.getName());
+            if(StrictMap.class.equals(objClass)){
+                StrictMap sm = (StrictMap) object;
+                List objList = (List) sm.get("list");
+                doInjectID(objList);
+            }
+            else if (object instanceof ParamMap){
+                ParamMap paramMap = (ParamMap) object;
+                Object insertEntity = paramMap.get("entity");
+                doInjectID(insertEntity);
+            }
+            else {
+                doInjectID(object);
+            }
+            ShardingThreadLocalContext.put(ShardingThreadLocalContext.ContextKeys.ColumnMatchType,ShardingColumnMatchType.FULL);
+        }
+        else if(SqlCommandType.SELECT.equals(sqlCommandType)
+//                || SqlCommandType.UPDATE.equals(sqlCommandType)
+//                || sqlCommandType.DELETE.equals(sqlCommandType)
+                ){
+            ShardingThreadLocalContext.put(ShardingThreadLocalContext.ContextKeys.ColumnMatchType,ShardingColumnMatchType.PART);
         }
     }
-
-    private void fillForEntityTime(Invocation invocation) {
+    
+    private void doInjectID(Object object) throws ArrayIndexOutOfBoundsException, IllegalArgumentException, IllegalAccessException, NoSuchFieldException, SecurityException{
+        Class objClass = object.getClass();
+        if(objClass.isArray()){
+            injectArrayID(object);
+        }
+        else if(List.class.isAssignableFrom(objClass)){
+            injectListID(object);
+        }
+        else {
+            injectData(object);
+        }
+    }
+    
+    
+    
+    private void injectArrayID(Object obj) throws ArrayIndexOutOfBoundsException,
+            IllegalArgumentException, IllegalAccessException, NoSuchFieldException, SecurityException {
+        int arrayLength = Array.getLength(obj);
+        for (int i = 0; i < arrayLength; i++) {
+            injectData(Array.get(obj, i));
+        }
+    }
+    
+    private void injectListID(Object obj) throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException, SecurityException{
+        List objList = (List) obj;
+        for(Object object : objList){
+            injectData(object);
+        }
+    }
+    
+    public void injectData(Object obj) throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException, SecurityException{
+        injectID(obj);
+        fillForEntityTime(obj);
+    }
+    
+    private void injectID(Object entity) throws IllegalArgumentException, IllegalAccessException, NoSuchFieldException, SecurityException{
+        Field idField = null;
         try {
-            MappedStatement mappedStatement = (MappedStatement) invocation.getArgs()[0];
-            Object object = null;
-            // sql类型
-            SqlCommandType sqlCommandType = mappedStatement.getSqlCommandType();
-            if (SqlCommandType.INSERT.equals(sqlCommandType)) {
-                object = invocation.getArgs()[1];
-                // 插入操作时，自动插入
-                long currentTimeInMills = CustomTimeOperatingUtils.currentTimeInMills();
-                Field fieldCreateTime = object.getClass().getDeclaredField("createTime");
-                fieldCreateTime.setAccessible(true);
-                fieldCreateTime.set(object, new Timestamp(currentTimeInMills));
+            idField = entity.getClass().getDeclaredField("id");
+        } catch (java.lang.NoSuchFieldException e) {
+            if(entity.getClass().getSuperclass() != null){
+                System.out.println(entity.getClass().getSuperclass());
+                idField = entity.getClass().getSuperclass().getDeclaredField("id");
             }
+        }
+        Table tableAnnotation = entity.getClass().getAnnotation(Table.class);
+        if (tableAnnotation == null || idField == null)
+            return;
+        idField.setAccessible(true);
+        Long id =  defaultKeyGenerator.generateKey().longValue();
+        //idField.set(object, dailySequenceManager.incr(tableAnnotation.name()));
+        idField.set(entity, id);
+    }
+
+    private void fillForEntityTime(Object  object) {
+        try {
+            long currentTimeInMills = CustomTimeOperatingUtils.currentTimeInMills();
+            Field fieldCreateTime = null;
+            try {
+                fieldCreateTime = object.getClass().getDeclaredField("createTime");
+            } catch (Exception e) {
+                if(object.getClass().getSuperclass() != null){
+                    fieldCreateTime = object.getClass().getSuperclass().getDeclaredField("createTime");
+                }
+            }
+            fieldCreateTime.setAccessible(true);
+            fieldCreateTime.set(object, new Timestamp(currentTimeInMills));
         } catch (Exception e) {
             logger.error(e.getMessage(), e);
         }
@@ -70,7 +149,6 @@ public class EntityOperatorInterceptor implements Interceptor {
     @Override
     public Object intercept(Invocation invocation) throws Throwable {
         fillIdForInsertEntity(invocation);
-        fillForEntityTime(invocation);
         Object result = invocation.proceed();
         return result;
     }
@@ -86,3 +164,4 @@ public class EntityOperatorInterceptor implements Interceptor {
     }
 
 }
+
